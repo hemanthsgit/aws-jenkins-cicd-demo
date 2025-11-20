28f2b24632be07de9396f275d200aa139e3d999e
M Hemanth <166197542+hemanthsgit@users.noreply.github.com>
2025-11-21 02:41:26 +0530

    Fix typo in response message

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from CI/CD on AWS via Jenkins & Docker!');
+  res.send('Hello from CI/CD on AWS via Jenkins & Dockekhkhkjhkiur!');
 });
 
 app.listen(port, () => {
