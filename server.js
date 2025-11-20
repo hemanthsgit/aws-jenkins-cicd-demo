f94f88a0268f1309979e106f73988bc17ff48210
M Hemanth <166197542+hemanthsgit@users.noreply.github.com>
2025-11-21 02:54:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from CI/CD on AWS via Jenkins & Dockekhkhkjhkiur!');
+  res.send('Hello from CI/CD on AWS via Jenkins & Dockek fsdfdvhkhkjhkiur!');
 });
 
 app.listen(port, () => {
